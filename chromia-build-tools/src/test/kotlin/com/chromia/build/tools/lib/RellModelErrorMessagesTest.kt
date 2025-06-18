439870988e6a1f4626eff524b8673a79e63a2ee6
Issame Zguiri <issame.zguiri@chromaway.com>
2025-06-18 10:00:41 +0000

    Update RellLibraryModel to work for both library-chain install and git clone installation

--- chromia-build-tools/src/test/kotlin/com/chromia/build/tools/lib/RellModelErrorMessagesTest.kt
+++ chromia-build-tools/src/test/kotlin/com/chromia/build/tools/lib/RellModelErrorMessagesTest.kt
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
 
-
 internal class RellModelErrorMessagesTest {
 
     @Test
@@ -31,6 +30,10 @@ internal class RellModelErrorMessagesTest {
         assertThat(throwable.message!!).contains("""
             Following errors found in chromia.yml:
             Additional property 'foo' found but was invalid (location: libs->foo)
+            Combination schema "oneOf" fails - 0 of 2 valid (location: libs->foo)
+            Additional property 'some_unexpected_field' found but was invalid (location: libs->foo->some_unexpected_field)
+            Required property "version" not found (location: libs->foo)
+            Additional property 'rid' found but was invalid (location: libs->foo->rid)
             Additional property 'some_unexpected_field' found but was invalid (location: libs->foo->some_unexpected_field)
         """.trimIndent())
     }
@@ -112,7 +115,15 @@ internal class RellModelErrorMessagesTest {
             }
         }
         val throwable = assertThrows<ValidationException> { parseModel(dir.resolve("chromia.yml").toFile()) }
-        assertThat(throwable.message!!).contains("Incorrect type, expected String (location: libs->foo->registry)")
+        assertThat(throwable.message!!).contains(
+            """
+                Following errors found in chromia.yml:
+                Additional property 'foo' found but was invalid (location: libs->foo)
+                Combination schema "oneOf" fails - 0 of 2 valid (location: libs->foo)
+                String doesn't match pattern ^(https?|ssh)://.*$ - "x\"1234\"" (location: libs->foo->registry)
+                Required property "version" not found (location: libs->foo)
+            """.trimIndent()
+        )
     }
 
     @Test
@@ -149,7 +160,10 @@ internal class RellModelErrorMessagesTest {
         assertThat(throwable.message!!).contains("""
             Following errors found in chromia.yml:
             Additional property 'foo' found but was invalid (location: libs->foo)
-            String doesn't match pattern ^x"[a-fA-F0-9]+"${'$'} - "615175A2847D739 ... 2A7E3C3E7032DB5" (location: libs->foo->rid)
+            Combination schema "oneOf" fails - 0 of 2 valid (location: libs->foo)
+            String doesn't match pattern ^x"[a-fA-F0-9]+"$ - "615175A2847D739 ... 2A7E3C3E7032DB5" (location: libs->foo->rid)
+            Required property "version" not found (location: libs->foo)
+            Additional property 'rid' found but was invalid (location: libs->foo->rid)
         """.trimIndent())
     }
 
@@ -213,8 +227,12 @@ internal class RellModelErrorMessagesTest {
         assertThat(throwable.message!!).contains("""
             Following errors found in chromia.yml:
             Additional property 'foo' found but was invalid (location: libs->foo)
+            Combination schema "oneOf" fails - 0 of 2 valid (location: libs->foo)
             Incorrect type, expected boolean (location: libs->foo->insecure)
-        """.trimIndent())
+            Required property "version" not found (location: libs->foo)
+            Additional property 'insecure' found but was invalid (location: libs->foo->insecure)
+            """.trimIndent()
+        )
     }
 
     @Test
