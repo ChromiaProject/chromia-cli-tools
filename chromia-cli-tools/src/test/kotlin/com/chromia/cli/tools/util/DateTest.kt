c5d4728c33eda84866dfde5af17d0532b58788a3
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-08-12 08:48:14 +0200

    Improve test

--- chromia-cli-tools/src/test/kotlin/com/chromia/cli/tools/util/DateTest.kt
+++ chromia-cli-tools/src/test/kotlin/com/chromia/cli/tools/util/DateTest.kt
@@ -2,20 +2,28 @@ package com.chromia.cli.tools.util
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
 import org.junit.jupiter.api.Test
 
 class DateTest {
 
     @Test
-    fun test() {
+    fun success() {
         listOf(
                 "2025-07-20 19:04",
                 "2025-07-20T19:04",
         ).forEach {
             assertThat(parseDateTimeAsEpochMillis(it)).isEqualTo(1753038240000)
         }
+    }
 
-        assertThat(parseDateTimeAsEpochMillis("2025-07-20 19:04")).isEqualTo(1753038240000)
+    @Test
+    fun failure() {
+        listOf(
+                "x2025-07-20 19:04",
+                "Wednesday, July 20, 2025 19:04",
+        ).forEach {
+            assertThat(parseDateTimeAsEpochMillis(it)).isNull()
+        }
     }
 }
-
