91749dbe1436f5ee4062bfb3bcd57666a4b9d9ce
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-11-11 14:32:01 +0100

    Remove unused and obsolete function

--- chromia-cli-tools/src/main/kotlin/com/chromia/cli/tools/multisignature/multisignature.kt
+++ chromia-cli-tools/src/main/kotlin/com/chromia/cli/tools/multisignature/multisignature.kt
@@ -3,27 +3,7 @@ package com.chromia.cli.tools.multisignature
 import com.chromia.build.tools.multisignature.MultiSignatureTxData
 import com.chromia.cli.tools.util.getFormattedUtcDateTime
 import com.github.ajalt.clikt.core.PrintMessage
-import net.postchain.common.PropertiesFileLoader
-import net.postchain.crypto.PubKey
 import java.io.File
-
-fun getSignersFromFile(file: File): Set<PubKey> {
-    val properties = PropertiesFileLoader.load(file.path)
-    val signers = mutableListOf<PubKey>()
-
-    val keys = properties.keys
-    while (keys.hasNext()) {
-        val key = keys.next()
-        val value = properties.getString(key)
-        try {
-            val pubkey = PubKey(value)
-            signers.add(pubkey)
-        } catch (e: IllegalArgumentException) {
-            throw PrintMessage("Failed to add signer for value: $value, reason: ${e.message}. Please verify that your signers file is defined correctly", 1)
-        }
-    }
-    return signers.toSet()
-}
 
 fun MultiSignatureTxData.Companion.parseTransactionFile(transactionFile: File): MultiSignatureTxData = try {
     decode(transactionFile.readText().trim())
