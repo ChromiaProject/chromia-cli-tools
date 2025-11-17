aa867d8cbc5ab6f15b896bca1f4abfce0a72ccc9
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-11-17 10:15:43 +0100

    Option for specifying transaction signers

--- chromia-cli-tools/src/main/kotlin/com/chromia/cli/tools/util/Options.kt
+++ chromia-cli-tools/src/main/kotlin/com/chromia/cli/tools/util/Options.kt
@@ -1,11 +1,17 @@
 package com.chromia.cli.tools.util
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.ParameterHolder
 import com.github.ajalt.clikt.parameters.groups.mutuallyExclusiveOptions
 import com.github.ajalt.clikt.parameters.groups.single
 import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.multiple
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.unique
+import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.long
+import net.postchain.common.hexStringToByteArray
+import net.postchain.crypto.PubKey
 import java.time.Clock
 
 fun CliktCommand.thresholdOption() = option(
@@ -28,3 +34,15 @@ fun CliktCommand.timebOptions(clock: Clock) = mutuallyExclusiveOptions(
 fun CliktCommand.slowDBStatementLogMsOption() = option("-sdbl", "--slow-db-statement-log-ms",
     help = "Threshold for slow DB statement log in milliseconds")
     .long()
+
+fun ParameterHolder.signersOption(name: String = "Signers, leave out this option to send transaction directly") = mutuallyExclusiveOptions(
+        option("--signer", help = "Public keys of signer (can be repeated)")
+                .convert { PubKey(it.hexStringToByteArray()) }
+                .multiple().unique(),
+        option("--signers", help = "Comma separated list of keys of signers")
+                .convert { s -> s.split(",").map { PubKey(it.hexStringToByteArray()) }.toSet() },
+        option("--signers-file", help = "Path to file containing public keys of signers, one per line")
+                .file(canBeDir = false, mustExist = true, mustBeReadable = true)
+                .convert { file -> file.readLines().map { PubKey(it.hexStringToByteArray()) }.toSet() },
+        name = name,
+).single()
