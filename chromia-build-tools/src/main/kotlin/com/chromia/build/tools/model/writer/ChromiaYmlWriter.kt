ba3b879471e76e4235267d9112955a0372f3ad66
Tim Steinholtz <tim.steinholtz@chromaway.com>
2026-02-18 15:52:31 +0100

    Move node utils into new file yamlNodeUtils.kt

--- chromia-build-tools/src/main/kotlin/com/chromia/build/tools/model/writer/ChromiaYmlWriter.kt
+++ chromia-build-tools/src/main/kotlin/com/chromia/build/tools/model/writer/ChromiaYmlWriter.kt
@@ -68,25 +68,3 @@ internal fun dumpYaml(yamlNode: Node, writer: BufferedWriter) {
     yaml.serialize(yamlNode, writer)
 }
 
-internal fun Node.isIncludeNode(): Boolean =
-    this is ScalarNode && this.tag == Tag("!include")
-
-internal fun Node.isScalarWithValue(value: String): Boolean =
-    this is ScalarNode && this.value == value
-
-internal fun createScalarNode(value: String): ScalarNode =
-    ScalarNode(Tag.STR, value, null, null, DumperOptions.ScalarStyle.PLAIN)
-
-internal fun MappingNode.getOrCreateMappingNode(key: String): MappingNode {
-    val existing = value.find { it.keyNode.isScalarWithValue(key) }?.valueNode
-
-    return when (existing) {
-        null -> MappingNode(Tag.MAP, mutableListOf(), DumperOptions.FlowStyle.BLOCK).also {
-            value.add(NodeTuple(createScalarNode(key), it))
-        }
-        is MappingNode -> existing
-        else -> throw InvalidChromiaModel(
-            "Expected '$key' to be a mapping node but found ${existing::class.simpleName}"
-        )
-    }
-}
