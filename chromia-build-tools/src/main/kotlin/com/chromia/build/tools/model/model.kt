6cb87118fea06622445ea7d9f2f6ef61d2aed7b1
Iaroslav Postovalov <iaroslav.postovalov@chromaway.com>
2026-08-07 06:51:51 +0200

    Bump Rell version to 0.16.6

--- chromia-build-tools/src/main/kotlin/com/chromia/build/tools/model/model.kt
+++ chromia-build-tools/src/main/kotlin/com/chromia/build/tools/model/model.kt
@@ -2,5 +2,5 @@ package com.chromia.cli.model
 
 import net.postchain.rell.base.model.R_LangVersion
 
-const val DefaultChromiaModelRellVersion = "0.16.5"
+const val DefaultChromiaModelRellVersion = "0.16.6"
 val MinimalRellVersionStrictGtv = R_LangVersion.of("0.13.9")
